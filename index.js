58605c717962d85e1aec87aa8e5550e8e6b63a67
Skuttm <Skutt_Michael@yahoo.com>
2022-11-11 17:17:44 -0500

    added space between console prints

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ function isStringNumber(string) {
     // cast the string to a number using '+'. If unable to cast, will return NaN.
     return !isNaN(+string);
 }
-console.log(isStringNumber("123")); // true
-console.log(isStringNumber("0.242")); // true
-console.log(isStringNumber("Code Test")); // false
-console.log(isStringNumber("10E30")); // true (exponential)
-console.log(isStringNumber("0x23F5")); // true (hexadecimal)
 // 2. Write a program to count the number of occurrences of a character in an input string
 function numCharacterOccurrences(string, char) {
     if (char.length !== 1) {
@@ -16,11 +11,6 @@ function numCharacterOccurrences(string, char) {
     }
     return string.split(char).length - 1;
 }
-console.log(numCharacterOccurrences("One Fish, Two Fish, Red Fish, Blue Fish", "F")); // 4
-console.log(numCharacterOccurrences("abbcccddddeeeee", "f")); // 0
-console.log(numCharacterOccurrences("abbcccddddeeeee", "b")); // 2
-console.log(numCharacterOccurrences("abbcccddddeeeee", "c")); // 3
-// console.log(numCharacterOccurrences("This results in an error", "this"));
 // 3. How to remove duplicates from an array
 function removeDuplicatesFromArray(array) {
     // in order to not mutate the original array
@@ -30,10 +20,6 @@ function removeDuplicatesFromArray(array) {
         return a;
     }, []);
 }
-console.log(removeDuplicatesFromArray([1, 1, 2, 2, 3, 4, 5])); // [1, 0, 2, 0, 3, 4, 5]
-console.log(removeDuplicatesFromArray([1, 1, 1, 1, 1, 1, 1])); // [1, 0, 0, 0, 0, 0, 0]
-console.log(removeDuplicatesFromArray([1, 2, 3, 4, 5, 6, 7])); // [1, 2, 3, 4, 5, 6, 7]
-console.log(removeDuplicatesFromArray([1, 2, 1, 1, 1, 1, 1])); // [1, 0, 0, 0, 0, 0, 2]
 // 4. Find the missing number in a given non-sorted integer array of 1 to 10
 function findMissingNumbers(array) {
     const result = [];
@@ -43,14 +29,9 @@ function findMissingNumbers(array) {
     }
     return result;
 }
-console.log(findMissingNumbers([1, 2, 3, 4, 6])); // [1, 0, 2, 0, 3, 4, 5]
-console.log(findMissingNumbers([1, 2, 3, 4, 6, 7, 9, 8, 10])); // [1, 0, 0, ]
-console.log(findMissingNumbers([1, 2, 3, 4, 6, 9, 8])); // [1, 0, 2, 0, 3, 4, 5]
-console.log(findMissingNumbers([1, 2, 3, 4, 9, 8])); // [1, 0, 2, 0, 3, 4, 5]
 // 5. Find all pairs in an array of integers whose sum is equal to a given number
 function findSumPairs(array, sum) {
     const result = [];
-    // remove duplicate numbers so duplicate results are eliminated (not mutating the parameter array)
     array.forEach((i, index) => {
         for (let j = index; j < array.length; j++) {
             if (i + j === sum)
@@ -59,4 +40,31 @@ function findSumPairs(array, sum) {
     });
     return result;
 }
+// Tests for 1
+console.log(isStringNumber("123")); // true
+console.log(isStringNumber("0.242")); // true
+console.log(isStringNumber("Code Test")); // false
+console.log(isStringNumber("10E30")); // true (exponential)
+console.log(isStringNumber("0x23F5")); // true (hexadecimal)
+console.log("\n\n");
+// Tests for 2
+console.log(numCharacterOccurrences("One Fish, Two Fish, Red Fish, Blue Fish", "F")); // 4
+console.log(numCharacterOccurrences("abbcccddddeeeee", "f")); // 0
+console.log(numCharacterOccurrences("abbcccddddeeeee", "b")); // 2
+console.log(numCharacterOccurrences("abbcccddddeeeee", "c")); // 3
+// console.log(numCharacterOccurrences("This results in an error", "this"));
+console.log("\n\n");
+// Tests for 3
+console.log(removeDuplicatesFromArray([1, 1, 2, 2, 3, 4, 5])); // [1, 0, 2, 0, 3, 4, 5]
+console.log(removeDuplicatesFromArray([1, 1, 1, 1, 1, 1, 1])); // [1, 0, 0, 0, 0, 0, 0]
+console.log(removeDuplicatesFromArray([1, 2, 3, 4, 5, 6, 7])); // [1, 2, 3, 4, 5, 6, 7]
+console.log(removeDuplicatesFromArray([1, 2, 1, 1, 1, 1, 1])); // [1, 0, 0, 0, 0, 0, 2]
+console.log("\n\n");
+// Tests for 4
+console.log(findMissingNumbers([1, 2, 3, 4, 6])); // [1, 0, 2, 0, 3, 4, 5]
+console.log(findMissingNumbers([1, 2, 3, 4, 6, 7, 9, 8, 10])); // [1, 0, 0, ]
+console.log(findMissingNumbers([1, 2, 3, 4, 6, 9, 8])); // [1, 0, 2, 0, 3, 4, 5]
+console.log(findMissingNumbers([1, 2, 3, 4, 9, 8])); // [1, 0, 2, 0, 3, 4, 5]
+console.log("\n\n");
+// Test for 5
 console.log(findSumPairs([2, 4, 3, 5, 6, -2, 4, 7, 8, 9], 7)); // [[2, 5], [4, 3], [3, 4], [-2, 9]]
--- index.ts
+++ index.ts
@@ -54,6 +54,7 @@ console.log(isStringNumber("0.242")); // true
 console.log(isStringNumber("Code Test")); // false
 console.log(isStringNumber("10E30")); // true (exponential)
 console.log(isStringNumber("0x23F5")); // true (hexadecimal)
+console.log("\n\n");
 
 // Tests for 2
 console.log(
@@ -63,18 +64,21 @@ console.log(numCharacterOccurrences("abbcccddddeeeee", "f")); // 0
 console.log(numCharacterOccurrences("abbcccddddeeeee", "b")); // 2
 console.log(numCharacterOccurrences("abbcccddddeeeee", "c")); // 3
 // console.log(numCharacterOccurrences("This results in an error", "this"));
+console.log("\n\n");
 
 // Tests for 3
 console.log(removeDuplicatesFromArray([1, 1, 2, 2, 3, 4, 5])); // [1, 0, 2, 0, 3, 4, 5]
 console.log(removeDuplicatesFromArray([1, 1, 1, 1, 1, 1, 1])); // [1, 0, 0, 0, 0, 0, 0]
 console.log(removeDuplicatesFromArray([1, 2, 3, 4, 5, 6, 7])); // [1, 2, 3, 4, 5, 6, 7]
 console.log(removeDuplicatesFromArray([1, 2, 1, 1, 1, 1, 1])); // [1, 0, 0, 0, 0, 0, 2]
+console.log("\n\n");
 
 // Tests for 4
 console.log(findMissingNumbers([1, 2, 3, 4, 6])); // [1, 0, 2, 0, 3, 4, 5]
 console.log(findMissingNumbers([1, 2, 3, 4, 6, 7, 9, 8, 10])); // [1, 0, 0, ]
 console.log(findMissingNumbers([1, 2, 3, 4, 6, 9, 8])); // [1, 0, 2, 0, 3, 4, 5]
 console.log(findMissingNumbers([1, 2, 3, 4, 9, 8])); // [1, 0, 2, 0, 3, 4, 5]
+console.log("\n\n");
 
 // Test for 5
 console.log(findSumPairs([2, 4, 3, 5, 6, -2, 4, 7, 8, 9], 7)); // [[2, 5], [4, 3], [3, 4], [-2, 9]]
